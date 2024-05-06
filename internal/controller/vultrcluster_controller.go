254a2b61111b871a2af3a1966d03b09d318f14ec
cmondragon <cmondragon.147@gmail.com>
2024-05-06 11:10:56 -0400

    add controller logic

--- internal/controller/vultrcluster_controller.go
+++ internal/controller/vultrcluster_controller.go
@@ -18,19 +18,34 @@ package controller
 
 import (
 	"context"
+	"fmt"
+	"time"
 
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/tools/record"
+
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	clusterv1 "sigs.k8s.io/cluster-api/api/v1beta1"
+	"sigs.k8s.io/cluster-api/util"
+	"sigs.k8s.io/cluster-api/util/annotations"
+	"sigs.k8s.io/cluster-api/util/conditions"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
-	infrav1 "github.com/mondragonfx/cluster-api-provider-vultr/api/v1"
+	"github.com/pkg/errors"
+	infrav1 "github.com/vultr/cluster-api-provider-vultr/api/v1"
+	"github.com/vultr/cluster-api-provider-vultr/cloud/scope"
 )
 
 // VultrClusterReconciler reconciles a VultrCluster object
 type VultrClusterReconciler struct {
 	client.Client
-	Scheme *runtime.Scheme
+	Scheme           *runtime.Scheme
+	ReconcileTimeout time.Duration
+	Recorder         record.EventRecorder
+	VultrApiKey      string
 }
 
 //+kubebuilder:rbac:groups=infra.cluster.x-k8s.io,resources=vultrclusters,verbs=get;list;watch;create;update;patch;delete
@@ -46,12 +61,94 @@ type VultrClusterReconciler struct {
 //
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.17.0/pkg/reconcile
-func (r *VultrClusterReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+func (r *VultrClusterReconciler) Reconcile(ctx context.Context, req ctrl.Request) (_ ctrl.Result, reterr error) {
+	// ctx, cancel := context.WithTimeout(ctx, reconciler.DefaultedLoopTimeout(r.ReconcileTimeout))
+	// defer cancel()
 
-	// TODO(user): your logic here
+	log := ctrl.LoggerFrom(ctx)
 
-	return ctrl.Result{}, nil
+	// Fetch the VultrCluster.
+	vultrCluster := &infrav1.VultrCluster{}
+	err := r.Get(ctx, req.NamespacedName, vultrCluster)
+	if err != nil {
+		if apierrors.IsNotFound(err) {
+			log.Info("VultrCluster resource not found or already deleted")
+			return ctrl.Result{}, nil
+		}
+		log.Error(err, "Unable to fetch VultrCluster resource")
+		return ctrl.Result{}, err
+	}
+
+	// Fetch the Cluster.
+	cluster, err := util.GetOwnerCluster(ctx, r.Client, vultrCluster.ObjectMeta)
+	if err != nil {
+		return reconcile.Result{}, fmt.Errorf("failed to get owner cluster: %w", err)
+	}
+	if cluster == nil {
+		log.Info("Cluster Controller has not yet set OwnerRef")
+		return reconcile.Result{}, nil
+	}
+	// Return early if the object or Cluster is paused.
+	if annotations.IsPaused(cluster, vultrCluster) {
+		log.Info("VultrCluster or linked Cluster is marked as paused. Won't reconcile")
+		return ctrl.Result{}, nil
+	}
+
+	// Create the cluster scope.
+	clusterScope, err := scope.NewClusterScope(ctx, r.VultrApiKey, scope.ClusterScopeParams{
+		Client:       r.Client,
+		Logger:       log,
+		Cluster:      cluster,
+		VultrCluster: vultrCluster,
+	})
+	if err != nil {
+		return ctrl.Result{}, errors.Errorf("failed to create scope: %v", err)
+	}
+
+	// Always close the scope when exiting this function so we can persist any changes.
+	defer func() {
+		err := clusterScope.Close()
+		if err != nil && reterr == nil {
+			reterr = err
+		}
+	}()
+
+	//Handle deleted clusters
+	if !vultrCluster.DeletionTimestamp.IsZero() {
+		return r.reconcileDelete(ctx, clusterScope)
+	}
+
+	// Handle non-deleted clusters
+	return r.reconcile(ctx, clusterScope)
+	//return ctrl.Result{}, nil
+
+}
+
+func (r *VultrClusterReconciler) reconcile(ctx context.Context, clusterScope *scope.ClusterScope) (res ctrl.Result, reterr error) {
+	res = ctrl.Result{}
+	// process controlplane endpoint
+	if clusterScope.VultrCluster.Spec.ControlPlaneEndpoint.Host == "" {
+
+		//TODO
+
+	}
+
+	clusterScope.VultrCluster.Status.Ready = true
+	conditions.MarkTrue(clusterScope.VultrCluster, clusterv1.ReadyCondition)
+
+	return res, nil
+
+}
+
+func (r *VultrClusterReconciler) reconcileDelete(ctx context.Context, clusterScope *scope.ClusterScope) (reconcile.Result, error) {
+	clusterScope.Info("Reconciling delete VultrCluster")
+	vultrcluster := clusterScope.VultrCluster
+
+	//TODO
+
+	// Cluster is deleted so remove the finalizer.
+	controllerutil.RemoveFinalizer(vultrcluster, infrav1.ClusterFinalizer)
+	return reconcile.Result{}, nil
 }
 
 // SetupWithManager sets up the controller with the Manager.
--- internal/controller/vultrmachine_controller.go
+++ internal/controller/vultrmachine_controller.go
@@ -19,18 +19,27 @@ package controller
 import (
 	"context"
 
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/cluster-api/util"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
-	infrav1 "github.com/mondragonfx/cluster-api-provider-vultr/api/v1"
+	"github.com/go-logr/logr"
+	"github.com/labstack/gommon/log"
+	"github.com/pkg/errors"
+	infrav1 "github.com/vultr/cluster-api-provider-vultr/api/v1"
+	"github.com/vultr/cluster-api-provider-vultr/cloud/scope"
 )
 
 // VultrMachineReconciler reconciles a VultrMachine object
 type VultrMachineReconciler struct {
 	client.Client
-	Scheme *runtime.Scheme
+	Scheme      *runtime.Scheme
+	Log         logr.Logger
+	VultrApiKey string
 }
 
 //+kubebuilder:rbac:groups=infra.cluster.x-k8s.io,resources=vultrmachines,verbs=get;list;watch;create;update;patch;delete
@@ -46,12 +55,120 @@ type VultrMachineReconciler struct {
 //
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.17.0/pkg/reconcile
-func (r *VultrMachineReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+// func (r *VultrMachineReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+// 	_ = log.FromContext(ctx)
 
-	// TODO(user): your logic here
+// 	// TODO(user): your logic here
 
-	return ctrl.Result{}, nil
+// 	return ctrl.Result{}, nil
+// }
+
+func (r *VultrMachineReconciler) Reconcile(ctx context.Context, req ctrl.Request) (_ ctrl.Result, reterr error) {
+	log := r.Log.WithValues("vultrmachine", req.NamespacedName)
+
+	// Fetch the VultrMachine.
+	vultrMachine := &infrav1.VultrMachine{}
+	err := r.Get(ctx, req.NamespacedName, vultrMachine)
+	if err != nil {
+		if apierrors.IsNotFound(err) {
+			//return ctrl.Result{}, nil
+			log.Error(err, "Failed to fetch VultrMachine")
+		}
+		return ctrl.Result{}, err
+	}
+
+	// Fetch the Machine.
+	machine, err := util.GetOwnerMachine(ctx, r.Client, vultrMachine.ObjectMeta)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+	if machine == nil {
+		log.Info("Machine Controller has not yet set OwnerRef")
+		return ctrl.Result{}, nil
+	}
+
+	log = r.Log.WithValues("VultrMachine", machine.Name)
+
+	// Fetch the Cluster.
+	cluster, err := util.GetClusterFromMetadata(ctx, r.Client, machine.ObjectMeta)
+	if err != nil {
+		log.Info("Machine is missing cluster label or cluster does not exist")
+		return ctrl.Result{}, nil
+	}
+
+	log = r.Log.WithValues("cluster", cluster.Name)
+
+	// Fetch the VultrCluster.
+	vultrCluster := &infrav1.VultrCluster{}
+	vultrClusterName := client.ObjectKey{
+		Namespace: vultrMachine.Namespace,
+		Name:      cluster.Spec.InfrastructureRef.Name,
+	}
+	if err := r.Client.Get(ctx, vultrClusterName, vultrCluster); err != nil {
+		log.Info("VultrCluster is not available yet.")
+		return ctrl.Result{}, nil
+	}
+
+	log = r.Log.WithValues("vultrCluster", vultrCluster.Name)
+
+	// // Retrieve the API key from environment variables
+	// apiKey := os.Getenv("VULTR_API_KEY")
+	// if apiKey == "" {
+	// 	return ctrl.Result{}, errors.New("environment variable VULTR_API_KEY is required")
+	// }
+
+	// Create the machine scope
+	machineScope, err := scope.NewMachineScope(ctx, r.VultrApiKey, scope.MachineScopeParams{
+		Client:       r.Client,
+		Logger:       log,
+		Cluster:      cluster,
+		Machine:      machine,
+		VultrCluster: vultrCluster,
+		VultrMachine: vultrMachine,
+	})
+	if err != nil {
+		return ctrl.Result{}, errors.Errorf("failed to create machine scope: %v", err)
+	}
+
+	defer func() {
+		err := machineScope.Close()
+		if err != nil && reterr == nil {
+			reterr = err
+		}
+	}()
+
+	if !vultrMachine.ObjectMeta.DeletionTimestamp.IsZero() {
+		return r.reconcileDelete(machineScope)
+	}
+
+	return r.reconcileNormal(ctx, machineScope)
+	//return ctrl.Result{}, nil
+
+}
+
+func (r *VultrMachineReconciler) reconcileNormal(ctx context.Context, machineScope *scope.MachineScope) (reconcile.Result, error) {
+	machineScope.Info("Reconciling VultrMachine")
+	vultrmachine := machineScope.VultrMachine
+
+	if vultrmachine.Status.FailureReason != nil || vultrmachine.Status.FailureMessage != nil {
+		machineScope.Info("Error state detected, skipping reconciliation")
+		return reconcile.Result{}, nil
+	}
+
+	// If the VultrMachine doesn't have our finalizer, add it.
+	controllerutil.AddFinalizer(machineScope.VultrMachine, infrav1.MachineFinalizer)
+
+	if !machineScope.Cluster.Status.InfrastructureReady {
+		machineScope.Info("Cluster infrastructure is not ready yet")
+		return reconcile.Result{}, nil
+	}
+
+	// Register the finalizer immediately to avoid orphaning Vultr resources on delete.
+	if err := machineScope.PatchObject(ctx); err != nil {
+		return reconcile.Result{}, err
+	}
+
+	return reconcile.Result{}, nil
 }
 
 // SetupWithManager sets up the controller with the Manager.
@@ -59,4 +176,14 @@ func (r *VultrMachineReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&infrav1.VultrMachine{}).
 		Complete(r)
+}
+
+func (r *VultrMachineReconciler) reconcileDelete(machineScope *scope.MachineScope) (reconcile.Result, error) {
+	log.Info("Reconciling Machine Delete")
+	vultrmachine := machineScope.VultrMachine
+
+	//TODO
+
+	controllerutil.RemoveFinalizer(vultrmachine, infrav1.MachineFinalizer)
+	return reconcile.Result{}, nil
 }
