f8d9b01a363d1c5d410f5b6bac62b5138f3224c0
cmondragon <cmondragon.147@gmail.com>
2024-06-21 11:25:26 -0400

    update cluster template vars

---
+++ scripts/capv-config.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#CAPVULTR  example configuration
+export KUBERNETES_VERSION=v1.30.0
+export CLUSTER_NAME="CAPVULTR-CLUSTER-TEST"
+export CONTROL_PLANE_MACHINE_COUNT=1
+export WORKER_MACHINE_COUNT=1
